d2f42c4d49c72f222195d8fc49ead771219ef7a4
17034106 <17034106@myrp.edu.sg>
2019-06-08 18:43:02 +0800

    update with comments

--- app/src/main/java/sg/edu/rp/c346/c300/GoalSavingAll.java
+++ app/src/main/java/sg/edu/rp/c346/c300/GoalSavingAll.java
@@ -3,11 +3,11 @@ package sg.edu.rp.c346.c300;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GoalSaving extends AppCompatActivity {
+public class GoalSavingAll extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goal_saving);
+        setContentView(R.layout.activity_goal_saving_all);
     }
 }
